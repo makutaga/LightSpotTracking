92604d3d1cccb7a0635ba2c0b43cbb92a9d90c54
Masatake Akutagawa <makutaga@icloud.com>
2021-01-30 19:48:03 +0900

    Just format improvement

--- markTracking.py
+++ markTracking.py
@@ -7,10 +7,13 @@ import sys
 import numpy as np
 import cv2
 
+import pyqtgraph as pg
+
 import cvcam
 
 DEFAULT_CAPTURE_DEVICE = 0
 DEFAULT_KERNEL_SIZE = 10
+
 
 def capture_display_cv(args):
     '''カメラ画像をキャプチャして画像処理し，画面に表示
@@ -22,15 +25,20 @@ def capture_display_cv(args):
     '''
     capture = cv2.VideoCapture(args.capture_device)  # キャプチャデバイスの指定
 
-    while(True):
-        ret, img = capture.read()
+    try:
+        while True:
+            ret, img = capture.read()
 
-        # q を押すと終了
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            print(img.shape)
+            # q を押すと終了
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    except KeyboardInterrupt:
+        pass
 
     capture.release()
     cv2.destroyAllWindows()
+
 
 def main(argv):
     parser = argparse.ArgumentParser()
